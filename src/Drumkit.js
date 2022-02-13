f61f9d22e59b646a1726ac2482e43818cbe0ae50
Mejsun <mejsunalghoul@gmail.com>
2022-02-13 15:23:04 +0000

    added sound

--- src/Drumkit.js
+++ src/Drumkit.js
@@ -2,20 +2,19 @@ import React, {useRef, useState} from 'react'
 
 function Drumkit() {
     const [displayName, setDisplayName] = useState();
-    //const [audiosrc, setAudiosrc] = useState();
-    
-    const audiosrc = useRef(null)
+    const [audiosrc, setAudiosrc] = useState('')
+    const audioref = useRef(null)
 
     const drumpads = [
-    {keyCode: 81, keyTrigger: 'Q', name: 'Heater-1', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
-    {keyCode: 87, keyTrigger: 'W', name: 'Heater-2', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
-    {keyCode: 69, keyTrigger: 'E', name: 'Heater-3', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
-    {keyCode: 65, keyTrigger: 'A', name: 'Heater-4', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
-    {keyCode: 83, keyTrigger: 'S', name: 'Clap', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
-    {keyCode: 68, keyTrigger: 'D', name: 'Open-HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
-    {keyCode: 90, keyTrigger: 'Z', name: "Kick-n'-Hat", url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
-    {keyCode: 88, keyTrigger: 'X', name: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
-    {keyCode: 67, keyTrigger: 'C', name: 'Closed-HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
+    {keyCode: 81, keyTrigger: 'Q', name: 'Heater-1', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
+    {keyCode: 87, keyTrigger: 'W', name: 'Heater-2', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
+    {keyCode: 69, keyTrigger: 'E', name: 'Heater-3', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
+    {keyCode: 65, keyTrigger: 'A', name: 'Heater-4', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
+    {keyCode: 83, keyTrigger: 'S', name: 'Clap', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
+    {keyCode: 68, keyTrigger: 'D', name: 'Open-HH', url: 'http://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
+    {keyCode: 90, keyTrigger: 'Z', name: "Kick-n'-Hat", url: 'http://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
+    {keyCode: 88, keyTrigger: 'X', name: 'Kick', url: 'http://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
+    {keyCode: 67, keyTrigger: 'C', name: 'Closed-HH', url: 'http://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
     ]
 
     const handleClicked = (e) => {
@@ -23,11 +22,9 @@ function Drumkit() {
         let name  = drumpads[i].name;
         let source = drumpads[i].url;
         if(e.target.innerText === drumpads[i].keyTrigger){
-        setDisplayName(name);
-        audiosrc.current.play(source)
-        console.log(audiosrc)
-        //setAudiosrc(source);
-        //source.play()
+            setDisplayName(name);
+            audioref.current.play();
+            setAudiosrc(source);
         }}} 
         
     return (
@@ -35,10 +32,10 @@ function Drumkit() {
             <h1> Play me! </h1>
             <div className="mainDisplay" id="display">
                 <div className='board'>
-                {drumpads.map((drumpad, i) => {return(
+                {drumpads.map((drumpad) => {return(
                     <div>
-                        <button className='drum-pad' key={Math.random()} id={drumpad.name} onClick={handleClicked}> {drumpad.keyTrigger} </button>               
-                        <audio className='clip' id={drumpad.keyTrigger} ref={audiosrc} src={drumpad.url} type="audio/mpeg"> </audio>
+                    <button onClick={handleClicked} className='drum-pad' key={Math.random()} id={drumpad.name} > {drumpad.keyTrigger} </button>                        
+                    <audio className='clip' id={drumpad.keyTrigger} src={audiosrc} ref={audioref} autoPlay> </audio>                 
                     </div>
                      )})}
                    </div>
