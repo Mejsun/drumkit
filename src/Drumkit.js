a6c7ee36635ca2b3ab2255fa5808609a590bd05c
Mejsun <mejsunalghoul@gmail.com>
2022-02-14 16:39:57 +0000

    drumkit

--- src/Drumkit.js
+++ src/Drumkit.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 
 function Drumkit() {
     const [displayName, setDisplayName] = useState();
@@ -6,15 +6,15 @@ function Drumkit() {
     const audioref = useRef(null)
 
     const drumpads = [
-    {keyCode: 81, keyTrigger: 'Q', name: 'Heater-1', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
-    {keyCode: 87, keyTrigger: 'W', name: 'Heater-2', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
-    {keyCode: 69, keyTrigger: 'E', name: 'Heater-3', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
-    {keyCode: 65, keyTrigger: 'A', name: 'Heater-4', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
-    {keyCode: 83, keyTrigger: 'S', name: 'Clap', url: 'http://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
-    {keyCode: 68, keyTrigger: 'D', name: 'Open-HH', url: 'http://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
-    {keyCode: 90, keyTrigger: 'Z', name: "Kick-n'-Hat", url: 'http://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
-    {keyCode: 88, keyTrigger: 'X', name: 'Kick', url: 'http://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
-    {keyCode: 67, keyTrigger: 'C', name: 'Closed-HH', url: 'http://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
+    {keyCode: 81, keyTrigger: 'Q', name: 'Tom', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tom.wav'},
+    {keyCode: 87, keyTrigger: 'W', name: 'Snare', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/snare.wav'},
+    {keyCode: 69, keyTrigger: 'E', name: 'Ride', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/ride.wav'},
+    {keyCode: 65, keyTrigger: 'A', name: 'Boom', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/boom.wav'},
+    {keyCode: 83, keyTrigger: 'S', name: 'Clap', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/clap.wav'},
+    {keyCode: 68, keyTrigger: 'D', name: 'Open-HH', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/openhat.wav'},
+    {keyCode: 90, keyTrigger: 'Z', name: 'Tink', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tink.wav'},
+    {keyCode: 88, keyTrigger: 'X', name: 'Kick', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/kick.wav'},
+    {keyCode: 67, keyTrigger: 'C', name: 'Closed-HH', url: 'http://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/hihat.wav'}
     ]
 
     const handleClicked = (e) => {
@@ -23,9 +23,30 @@ function Drumkit() {
         let source = drumpads[i].url;
         if(e.target.innerText === drumpads[i].keyTrigger){
             setDisplayName(name);
-            audioref.current.play();
             setAudiosrc(source);
+            audioref.current.play();
+            audioref.current.time = 0;
+            console.log(source)
         }}} 
+
+    
+    const handleKeyPress = (e) => {
+        for(let i=0; i<drumpads.length; i++){
+        let name  = drumpads[i].name;
+        let source = drumpads[i].url;
+        let keyid = drumpads[i].keyCode
+        if(e.keyCode === keyid){
+        setDisplayName(name);
+        setAudiosrc(source);
+        audioref.current.play();
+        audioref.current.time = 0;
+        console.log(source)
+        }}
+   }
+    useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {window.removeEventListener('keydown', handleKeyPress)};
+    });
         
     return (
         <div className="App" id="drum-machine" >
@@ -33,16 +54,15 @@ function Drumkit() {
             <div className="mainDisplay" id="display">
                 <div className='board'>
                 {drumpads.map((drumpad) => {return(
-                    <div>
-                    <button onClick={handleClicked} className='drum-pad' key={Math.random()} id={drumpad.name} > {drumpad.keyTrigger} </button>                        
+                    <button onClick={handleClicked} className='drum-pad' key={Math.random()} id={drumpad.name} > {drumpad.keyTrigger}                      
                     <audio className='clip' id={drumpad.keyTrigger} src={audiosrc} ref={audioref} autoPlay> </audio>                 
-                    </div>
+                    </button>
                      )})}
                    </div>
                 <div className= 'secondscreen'>{displayName}</div>
             </div>
         </div>
-          );
-    }
+    );
+}
 
 export default Drumkit
